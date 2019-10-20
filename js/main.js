9adb17d7406401193e4e4a318b5747dd965acd33
Your Name <you@example.com>
2019-10-20 00:08:28 -0300

    Treino com JavaScript

---
+++ js/main.js
@@ -0,0 +1,124 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por Clicar</b>";
+}
+
+function mudar(){
+    window.open ("http://escolainterlibras.com.br/");
+    windown.location.href = "http://escolainterlibras.com.br/";
+}
+
+function trocar(){
+    elemento.innerHTML="Obrigado por passar o mouse";
+    //document.getElementById("mousemove").innerHTML="Obrigado por passar o mouse";
+    // alert("trocar texto");
+}
+
+function voltar(){
+    elemento.innerHTML="Passe o mouse aqui";
+    //document.getElementById("mousemove").innerHTML="Passe o mouse aqui";
+    // alert("trocar texto");
+}
+
+/*
+function clicou(){
+    alert("Obrigado pelo clique");
+}
+*/
+
+/*
+function soma (n1,n2){
+    return n1+n2;
+}
+
+function validadeIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else {
+        validar = false
+    }
+    return validar;
+}   
+
+var idade = prompt ("Qual a sua idade?");
+console.log(frutas);
+*/
+
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <=5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade =prompt("Qual a sua idade?"); 
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");    
+}
+*/
+
+/*
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta);
+alert(fruta.cor);
+*/
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+
+//console.log(lista.length);
+//quantos itens na lita
+
+//console.log(lista.reverse);
+//inverte a ordem da lista
+
+//console.log(lista.toString());
+//traz como string
+
+//console.log(lista.join("-"));
+//join troca pelo que o desenvolver colocar
+
+
+/*
+var nome ="Kaique Fernandes";
+var n1 = 10;
+var n2 = 2;
+var frase="Japão é o melhor time do mundo"
+alert(nome + " tem " + idade + " anos");
+alert(idade + idade2);
+console.log(nome);
+console.log(n1+n2);
+console.log(n1-n2);
+console.log(n1/n2);
+console.log(n1*n2);
+console.log(frase.toUpperCase());
+alert(frase.replace("Japão", "Brasil"));
+*/
